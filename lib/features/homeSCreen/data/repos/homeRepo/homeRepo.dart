41bfed16eae6b79e14417a512b7e192c360d682b
Mahmoud Ahmed <103232226+MahmoudAhmed2003@users.noreply.github.com>
2024-10-20 16:19:17 +0300

    add dartz and create failure abstract

--- lib/features/homeSCreen/data/repos/homeRepo/homeRepo.dart
+++ lib/features/homeSCreen/data/repos/homeRepo/homeRepo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/homeScreen/data/models/booksModel/books_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class Homerepo {
-   getBestSellerBooks();
-   getFeaturedBooks();
+  Future<Either<Failure, List<BooksModel>>> getBestSellerBooks();
+  Future<Either<Failure, List<BooksModel>>> getFeaturedBooks();
 }
