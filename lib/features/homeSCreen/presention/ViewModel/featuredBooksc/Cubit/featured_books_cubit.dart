59fa3238346a1502cca4d5d58f18af6affec2377
Mahmoud Ahmed <103232226+MahmoudAhmed2003@users.noreply.github.com>
2024-10-24 00:50:58 +0300

    Refactor featured_books_cubit to use homeRepo for fetching featured books

--- lib/features/homeSCreen/presention/ViewModel/featuredBooksc/Cubit/featured_books_cubit.dart
+++ lib/features/homeSCreen/presention/ViewModel/featuredBooksc/Cubit/featured_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/homeScreen/data/models/booksModel/books_model.dart';
+import 'package:bookly_app/features/homeScreen/data/repos/homeRepo.dart';
+import 'package:bookly_app/features/homeScreen/data/repos/homeRepoImpl.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homerepo) : super(FeaturedBooksInitial());
+  final Homerepo homerepo;
+  Future<void> getFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    final result = await homerepo.getFeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturedBooksFailed(message: failure.errMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books: books));
+    });
+  }
 }
