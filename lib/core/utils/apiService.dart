3a7f01434ec7493ef74bb9bd4a45013e630a850e
Mahmoud Ahmed <103232226+MahmoudAhmed2003@users.noreply.github.com>
2024-10-23 16:43:07 +0300

    handling server Failure error

--- lib/core/utils/apiService.dart
+++ lib/core/utils/apiService.dart
@@ -2,11 +2,11 @@ import 'package:dio/dio.dart';
 
 class Apiservice {
   final _baseUrl = 'https://www.googleapis.com/books/v1/';
-  final Dio dio;
-  Apiservice(this.dio);
+  final Dio _dio;
+  Apiservice(this._dio);
 
    Future<Map<String, dynamic>> get({required String endPoint})async {
-    final response =await dio.get(_baseUrl + endPoint);
+    final response =await _dio.get(_baseUrl + endPoint);
 
     return response.data;
   }
